5881e13d192b7c3791d380a6dcb2afac7f85dc29
21539288 <alomac17@gmail.com>
2017-10-05 18:18:29 +0200

    21539288

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -19,6 +21,36 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    //geolocation
+    constructor(private backgroundGeolocation: BackgroundGeolocation) { }
+
+    const config: BackgroundGeolocationConfig = {
+                desiredAccuracy: 10,
+                stationaryRadius: 20,
+                distanceFilter: 30,
+                debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+                stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+        };
+
+    this.backgroundGeolocation.configure(config)
+      .subscribe((location: BackgroundGeolocationResponse) => {
+
+        console.log(location);
+
+        // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+        // and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+        // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+        this.backgroundGeolocation.finish(); // FOR IOS ONLY
+
+      });
+
+    // start recording location
+    this.backgroundGeolocation.start();
+
+    // If you wish to turn OFF background-tracking, call the #stop method.
+    this.backgroundGeolocation.stop();
+    //end geolocation
+
   }
 }
-
